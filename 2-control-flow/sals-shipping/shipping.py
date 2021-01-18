2b317ff0c1fcd011371051dd84823f47a2e8e9a3
Sonny Li <sonnynomnom@gmail.com>
2021-01-18 17:38:16 -0500

    updates

---
+++ 2-control-flow/sals-shipping/shipping.py
@@ -0,0 +1,36 @@
+# Sal's Shipping
+# Sonny Li
+
+weight = 80
+
+# Ground Shipping 🚚
+
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight <= 10:
+  cost_ground = weight * 4.00 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+
+print("Ground Shipping $", cost_ground)
+      
+# Ground Shipping Premimum 🚚💨
+
+cost_ground_premium = 125.00
+
+print("Ground Shipping Premimium $", cost_ground_premium)
+
+# Drone Shipping 🛸
+
+if weight <= 2:
+  cost_drone = weight * 4.5
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+
+print("Drone Shipping: $", cost_drone)
---
+++ 2-control-flow/space.py
@@ -0,0 +1,24 @@
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+# Write an if statement below:
+
+if planet == 1:
+  weight = weight * 0.78
+elif planet == 2:
+  weight = weight * 0.39
+elif planet == 3:
+  weight = weight * 2.65
+elif planet == 4:
+  weight = weight * 1.17
+elif planet == 5:
+  weight = weight * 1.05
+elif planet == 6:
+  weight = weight * 1.23
+
+print("Your weight:", weight)
