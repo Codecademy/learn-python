547bdc94aed5a1aec7a09c258c0bae9a32356160
Sonny Li <sonnynomnom@gmail.com>
2021-01-20 13:54:00 -0500

    Update block_letters.py

--- 1-hello-world/block-letters/block_letters.py
+++ 1-hello-world/block-letters/block_letters.py
@@ -1,13 +1,12 @@
 # My name is Shubham Kumar Tripathi.
 # I am a very honest person.
 
-name = ''' 
-             SSS   K   K TTTTT
-            S   S  K  K    T
-            S      K K     T
-             SSS   KK      T
-                S  K K     T
-            S   S  K  K    T
-             SSS   K   K   T  '''
+name = '''  SSS   K   K TTTTT
+           S   S  K  K    T
+           S      K K     T
+            SSS   KK      T
+               S  K K     T
+           S   S  K  K    T
+            SSS   K   K   T  '''
 
 print(name)
