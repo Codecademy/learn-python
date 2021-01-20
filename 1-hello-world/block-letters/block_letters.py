e73fcba0af585ed98f946e9d4682e4efb9a1eef6
Sonny Li <sonnynomnom@gmail.com>
2021-01-20 13:48:21 -0500

    Update block_letters.py

--- 1-hello-world/block-letters/block_letters.py
+++ 1-hello-world/block-letters/block_letters.py
@@ -1,11 +1,13 @@
-#My first Name is: Shubham , Middle Name is: Kumar, and Last Name is: Tripathi.
-#I am a very honest person.
-name =     ''' 
-                 SSS   K   K TTTTT
-                S   S  K  K    T
-                S      K K     T
-                 SSS   KK      T
-                    S  K K     T
-                S   S  K  K    T
-                 SSS   K   K   T  '''
-print (name)   
+# My name is Shubham Kumar Tripathi.
+# I am a very honest person.
+
+name = ''' 
+             SSS   K   K TTTTT
+            S   S  K  K    T
+            S      K K     T
+             SSS   KK      T
+                S  K K     T
+            S   S  K  K    T
+             SSS   K   K   T  '''
+
+print(name)
