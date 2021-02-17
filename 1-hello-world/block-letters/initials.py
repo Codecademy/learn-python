802ff4dae262ffe27f39e5b35f778ba1eb80991b
Felipe Tersariol <41866564+FelipeTersariol@users.noreply.github.com>
2021-02-17 20:12:06 -0300

    Update initials.py

--- 1-hello-world/block-letters/initials.py
+++ 1-hello-world/block-letters/initials.py
@@ -8,3 +8,17 @@ print("  SSS   L     ")
 print("     S  L     ")
 print(" S   S  L     ")
 print("  SSS   LLLLL ")
+
+# You also can do it like this:
+
+array1 = ["F ", "F ", "F ", "F ", "F "]
+array2 = ["F ", "F ", "F ", "  ", "  "]
+array3 = ["F ", "  ", "  ", "  ", "  "]
+
+matrix = [array1, array3, array3, array2, array3, array3, array3]
+
+for i in range(len(matrix)):
+  linha_print = ""
+  for w in range(len(matrix[i])):
+    linha_print += matrix[i][w]
+  print(linha_print)
