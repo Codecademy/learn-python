13f2b775e5bfb040262f8f19821b5abb355f387a
dawable <58328332+dawable@users.noreply.github.com>
2021-08-10 23:22:05 -0500

    Update magic 8 ball with optional questions

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,41 @@
 import random
 
-name = "Joe"
-question = "Will I win the lottery?"
+name = ""
+question = ""
 answer = ""
 
-random_number = random.randint(1, 9)
-# print(random_number)
+random_number = random.randint(1,9)
+#print(random_number)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Yes - definitely."
 elif random_number == 2:
   answer = "It is decidedly so"
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = "Without a doubt."
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Reply hazy, try again."
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Ask again later."
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Better not tell you now."
 elif random_number == 7:
-  answer = "My sources say no"
+  answer = "My sources say no."
 elif random_number == 8:
-  answer = "Outlook not so good"
+  answer = "Outlook not so good."
 elif random_number == 9:
-  answer = "Very doubtful"
-else:
+  answer = "Very doubtful."
+else: 
   answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+
+if name == "":
+  print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+
+if question == "":
+  print("Magic 8-ball cannot provide a fortune")
+else:
+  print(name + " asks: " + question)
+  print("Magic 8-Ball's answer: " + answer)
+
