1e98a3e4c5c4bd4ce67151137dd4796719a37792
dantoms <97520358+dantoms@users.noreply.github.com>
2022-02-11 10:51:45 +0000

    Update magic8.py
    This is how I solved the project in a slightly different way

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,31 @@
 import random
 
-name = "Joe"
-question = "Will I win the lottery?"
-answer = ""
+#list of answers for program to output
+answers = [
+    "Yes - definitely.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+]
 
-random_number = random.randint(1, 9)
-# print(random_number)
+#set variables to empty strings to facilitate while loops
+name = ""
+question = ""
 
-if random_number == 1:
-  answer = "Yes - definitely"
-elif random_number == 2:
-  answer = "It is decidedly so"
-elif random_number == 3:
-  answer = "Without a doubt"
-elif random_number == 4:
-  answer = "Reply hazy, try again"
-elif random_number == 5:
-  answer = "Ask again later"
-elif random_number == 6:
-  answer = "Better not tell you now"
-elif random_number == 7:
-  answer = "My sources say no"
-elif random_number == 8:
-  answer = "Outlook not so good"
-elif random_number == 9:
-  answer = "Very doubtful"
-else:
-  answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+#ensure user input
+while name == "":
+    name = input("What is your name?   ")
+while question == "":
+    question = input("What is your question?   ")
+
+#pick a random answer from list of answers above
+answer = answers[random.randint(0,len(answers))]
+
+#output it all
+print(name, "asks:", question)
+print("Magic 8-Ball's answer:", answer)
