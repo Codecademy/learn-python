ed22e56c7a4258307c79ecfcdd3ec965337a8f8e
Bhavy Yadav <51322577+Bhavyyadav25@users.noreply.github.com>
2022-07-27 20:17:59 +0530

    Update magic8.py

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,35 @@
-import random
-
-name = "Joe"
-question = "Will I win the lottery?"
+name = "Kirti"
+question = "Tomorrow will rain?"
 answer = ""
+import random
+random_number = random.randint(1,9)
+a = random_number
 
-random_number = random.randint(1, 9)
-# print(random_number)
-
-if random_number == 1:
-  answer = "Yes - definitely"
-elif random_number == 2:
-  answer = "It is decidedly so"
-elif random_number == 3:
-  answer = "Without a doubt"
-elif random_number == 4:
-  answer = "Reply hazy, try again"
-elif random_number == 5:
-  answer = "Ask again later"
-elif random_number == 6:
-  answer = "Better not tell you now"
-elif random_number == 7:
-  answer = "My sources say no"
-elif random_number == 8:
-  answer = "Outlook not so good"
-elif random_number == 9:
-  answer = "Very doubtful"
+if a == 1:
+  answer = "Yes - definitely."
+elif a == 2:
+  answer = "It is decidedly so."
+elif a == 3:
+  answer = "Without a doubt."
+elif a == 4:
+  answer = "Reply hazy, try again."
+elif a == 5:
+  answer = "Ask again later."
+elif a == 6:
+  answer = "Better not tell you now."
+elif a == 7:
+  answer = "My sources say no."
+elif a == 8:
+  answer = "Outlook not so good."
+elif a == 9:
+  answer = "Very doubtful."
 else:
   answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+
+if name == "":
+  print("Question: ", question)
+elif question == "":
+  print("Ask something or fuck off!")
+else:
+  print(name ," asks: ", question)
+  print("Magic 8-Ball's answer: ", answer)
