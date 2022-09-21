869ad64eb83024071d97770033763b00cc82ded2
Khaled Hany Genina <57587600+khaledhanyG@users.noreply.github.com>
2022-09-21 11:59:01 +0300

    Update magic8.py

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,24 +1,25 @@
 import random
+# create random number from 1 to 9
+random_number = random.randint(1, 9)
 
-name = "Joe"
-question = "Will I win the lottery?"
+# taking user name input and question
+name = input("Whats your name? ")
+question = input("Enter your question ...")
+# variables will be used later in the if statement
 answer = ""
 
-random_number = random.randint(1, 9)
-# print(random_number)
-
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Yes definitely."
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "It is decidedly so."
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = "Without a doubt."
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Reply hazy, try again."
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Ask again later."
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Better not to tell you now."
 elif random_number == 7:
   answer = "My sources say no"
 elif random_number == 8:
@@ -27,6 +28,6 @@ elif random_number == 9:
   answer = "Very doubtful"
 else:
   answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+
+print(name, " asks: ", question)
+print("Magic ball answers: ", answer)
