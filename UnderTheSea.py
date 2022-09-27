52a5f70332fc15b7e998e937f413838539c2b992
marisaaleigh <48932840+marisaaleigh@users.noreply.github.com>
2022-09-27 16:30:35 -0400

    UnderTheSea.py
    Make ASCII art using block letters :)

---
+++ UnderTheSea.py
@@ -0,0 +1,23 @@
+# Under the Sea Theme
+
+# Marisa Huber
+# Student, Game Design
+
+# TODO: Make an octopus & fish friendo.
+
+print()
+print()
+print("                   *                                                            ")
+print("                                      *                                         ")
+print("   *                                               *                            ")
+print("              LL         *      LLL                  *       FFFFF              ")
+print("            L  L               L   LL              *      FFFF      F           ")
+print("              L    OOOOOOOOO    L      *            *   F(o)FFFF  FF            ")
+print("     *       L    OOOOOOOOOOO    L                    =FFFFFFFFFFFF             ")
+print("        LLL   L  OOO{}OOO{}OOO  L   LLL                 FFFFFF    FF            ")
+print("       L   L   L  OOOOOOOOOOO  L   L   L                 F  FF      F           ")
+print("            L   L  OOO(o)OOOO  L   L         *            F  FF                 ")
+print("          *   LLLLLL OOOOO  LLLLLL                            F                 ")
+print("               LLLLLLLLLLLLLLLLLL                                               ")
+print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
