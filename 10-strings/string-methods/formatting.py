ac78b6645f37ec7f8366e20f216fabfa2d40e4fc
Leo Zeng <qingchun1995@gmail.com>
2022-11-14 22:22:26 +0800

    feat: format and split strings 1

---
+++ 10-strings/string-methods/formatting.py
@@ -0,0 +1,17 @@
+favorite_song = "SmOoTH"
+favorite_song_lowercase = favorite_song.lower()
+
+print(favorite_song)
+print(favorite_song_lowercase)
+
+poem_title = "spring storm"
+poem_title_fixed = poem_title.title()
+
+poem_author = "William Carlos Williams"
+poem_author_fixed = poem_author.upper()
+
+print(poem_title)
+print(poem_title_fixed)
+
+print(poem_author)
+print(poem_author_fixed)
