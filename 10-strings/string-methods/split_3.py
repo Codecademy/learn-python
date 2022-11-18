67c8a29587b30d779d855162cfd9615016b483bf
Leo Zeng <qingchun1995@gmail.com>
2022-11-18 22:49:54 +0800

    feat: strings

---
+++ 10-strings/string-methods/split_3.py
@@ -0,0 +1,29 @@
+smooth_chorus = \
+    """And if you said, "This life ain't good enough."
+I would give my world to lift you up
+I could change my life to better suit your mood
+Because you're so smooth"""
+chorus_lines = smooth_chorus.split('\n')
+
+print(chorus_lines)
+
+spring_storm_text = \
+    """The sky has given over
+its bitterness.
+Out of the dark change
+all day long
+rain falls and falls
+as if it would never end.
+Still the snow keeps
+its hold on the ground.
+But water, water
+from a thousand runnels!
+It collects swiftly,
+dappled with black
+cuts a way for itself
+through green ice in the gutters.
+Drop after drop it falls
+from the withered grass-stems
+of the overhanging embankment."""
+spring_storm_lines = spring_storm_text.split('\n')
+print(spring_storm_lines)
---
+++ demo.py
@@ -0,0 +1,4 @@
+game = "Popular Nintendo Game: Mario Kart"
+
+print("l" in game)
+print("x" in game)
