7ceaf9a8575ef02e195baa9e24ebbcf1f08a5d0e
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2022-12-23 12:12:14 -0500

    Update shipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,63 @@
-# Sal's Shipping
-# Sonny Li
+#Sal's Shipping
 
-weight = 80
+package_weight = 4.8;
 
-# Ground Shipping 🚚
+# check ground
+def check_ground(package_weight):
+  ground = 0;
 
-if weight <= 2:
-  cost_ground = weight * 1.5 + 20
-elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
-elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
-else:
-  cost_ground = weight * 4.75 + 20
+  ground += 20;
+  if package_weight <= 2:
+    ground += package_weight*1.5;
+  elif package_weight > 2 and package_weight <= 6:
+    ground += package_weight*3;
+  elif package_weight > 6 and package_weight <= 10:
+    ground += package_weight*4;
+  else:
+    ground += package_weight*4.75;
+  return ground;
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
+# check drone
+def check_drone(package_weight):
+  drone = 0;
 
-cost_ground_premium = 125.00
+  if package_weight <= 2:
+    drone += package_weight*4.5;
+  elif package_weight > 2 and package_weight <= 6:
+    drone += package_weight*9;
+  elif package_weight > 6 and package_weight <= 10:
+    drone += package_weight*12;
+  else:
+    drone += package_weight*14.25;
+  return drone;
 
-print("Ground Shipping Premimium $", cost_ground_premium)
 
-# Drone Shipping 🛸
+def final_shipping(package_weight):
+  ground = check_ground(package_weight);
+  ground_premium = 125;
+  drone = check_drone(package_weight);
 
-if weight <= 2:
-  cost_drone = weight * 4.5
-elif weight <= 6:
-  cost_drone = weight * 9.00
-elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
-  cost_drone = weight * 14.25
+  cheapest_option = "";
+  cheapest_price = 0;
+  
+  if ground < ground_premium and ground < drone:
+    cheapest_option = "Ground Shipping";
+    cheapest_price += ground; 
+  elif ground_premium < ground and ground_premium < drone:
+    cheapest_option = "Ground Shipping Premium";
+    cheapest_price += ground_premium;
+  else:
+    cheapest_option = "Drone Shipping";
+    cheapest_price += drone;
 
-print("Drone Shipping: $", cost_drone)
+  # Print final cheapest option
+  print("Ground Shipping is: $" + format(ground, ".2f"));
+  print("Ground Shipping Premium is: $" + format(ground_premium, ".2f"));
+  print("Drone Shipping is: $" + format(drone, ".2f"));
+  print("======================")
+  print("The cheapest option is " + cheapest_option + ". The cost to ship your " + str(package_weight) + "lb package, will be $" + format(cheapest_price, ".2f") + ".");
+
+final_shipping(package_weight);
+
+
+    
