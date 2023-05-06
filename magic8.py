913ff9c0010fe1eddcde39dd2401fd880f9428f5
umraffer32 <132298585+umraffer32@users.noreply.github.com>
2023-05-05 18:05:10 -0700

    Update magic8.py

--- magic8.py
+++ magic8.py
@@ -1,10 +1,9 @@
-############################################
 import random
 name = "Uriah" # Enter name here or leave as a blank string
 question = "Will learning Python 3 be easy?"
 answer = ""
 random_number = random.randint(1,13) 
-#############################################
+
 if random_number == 1:
   answer = "Yes, definitely."
 elif random_number == 2:
@@ -25,13 +24,7 @@ elif random_number == 9:
   answer = "Very doubtful."
 elif random_number == 10:
   answer = "Not a chance in hell."
-#################################################
-#################################################
-# This code will throw an "ERROR" message if the random number 
-# generated is 11, 12, or 13. It will also check the name to see
-# if it is a blank string or "Uriah"
-# If the name is "Uriah" it will say that name in the terminal
-# If the name is blank, it will say "Question: "
+
 if random_number >= 11:
   print ("ERROR")
 elif name == "":
@@ -40,4 +33,3 @@ elif name == "":
 elif random_number <= 10:
   print (name + " asks: " + question) 
   print ("Magic 8-Ball's answer: " + answer)
-################################################## 
