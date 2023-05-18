13ccec45c8f0ae944f4afb16975c97cbc50d5bac
loctogan <91305063+loctogan@users.noreply.github.com>
2023-05-18 16:43:37 +0200

    Update shipping.py with user input

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,48 @@
 # Sal's Shipping
-# Sonny Li
+# Loctogan
 
 weight = 80
-
-# Ground Shipping 🚚
-
+weight=int(input("Combien pèse votre colis?"))
+# Livraison par route 🚚
+flat_charge = 20
 if weight <= 2:
-  cost_ground = weight * 1.5 + 20
+  cost_ground = weight * 1.5 + flat_charge
 elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
+  cost_ground = weight * 3.00 + flat_charge
 elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
+  cost_ground = weight * 4.00 + flat_charge
 else:
-  cost_ground = weight * 4.75 + 20
+  cost_ground = weight * 4.75 + flat_charge
 
 print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
 
-cost_ground_premium = 125.00
+# Livraison premium 🚚
 
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
+cost_premium_shipping = 125
+print("Premium Shipping $", cost_premium_shipping)
+# drone Shipping 🚁
 
 if weight <= 2:
   cost_drone = weight * 4.5
 elif weight <= 6:
-  cost_drone = weight * 9.00
+  cost_drone = weight * 9
 elif weight <= 10:
-  cost_drone = weight * 12.00
+  cost_drone = weight * 12
 else:
   cost_drone = weight * 14.25
 
-print("Drone Shipping: $", cost_drone)
+print("Drone Shipping $", cost_drone)
+
+meilleure_offre = 0
+type_livraison= "aucune"
+if cost_ground<cost_drone and cost_drone<cost_premium_shipping:
+  meilleure_offre=cost_ground
+  type_livraison="standard"
+elif cost_drone<cost_ground and cost_drone<cost_premium_shipping:
+  meilleure_offre=cost_drone
+  type_livraison="par drone"
+else:
+  meilleure_offre=cost_premium_shipping
+  type_livraison="premium"
+
+print ("La meilleur offre est la livraison ",type_livraison,"et coûte",meilleure_offre,".- chfs")
