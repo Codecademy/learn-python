2324d7e1eb5ed37e7c723f2f331483efdb0d7e6a
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2023-12-05 14:43:22 -0400

    Initials.py

---
+++ Initials.py
@@ -0,0 +1,9 @@
+# Antonio Jose Villarreal
+# I am a really focus person
+print("  A  " " " "JJJJJ" " " "V   V")
+print(" A A " " " "  J  " " " "V   V")
+print("A   A" " " "  J  " " " "V   V")
+print("AAAAA" " " "  J  " " " "V   V")
+print("A   A" " " "J J  " " " "V   V")
+print("A   A" " " "J J  " " " " V V ")
+print("A   A" " " " JJ  " " " "  V  ")
